d0681fc6da865575850534f6d1004b51c99ff561
DayaneKaroline <dayanefalves33@gmail.com>
2026-03-26 21:23:17 -0300

    at

--- aula8/src/routes/validarUsuarios.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const router= express.Router();
-const validarUsuario = require('../controllers/usuarioController');
-const { validarUsuario } = require('../controllers/usuarioControllers');
-
-router.post('/', validarUsuario.listarUsuarios);
-
-module.exports= router;
